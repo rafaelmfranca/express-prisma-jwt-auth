f56a06b598ce86b6c2bff0f3f710b1551359e9ab
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-22 20:35:32 -0300

    feat(prisma): add db seed

---
+++ prisma/seed.ts
@@ -0,0 +1,51 @@
+import prisma from '../src/lib/prisma';
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      posts: {
+        create: {
+          title: 'Check out Prisma with Next.js',
+          content: 'https://www.prisma.io/nextjs',
+          published: true,
+        },
+      },
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      posts: {
+        create: [
+          {
+            title: 'Follow Prisma on Twitter',
+            content: 'https://twitter.com/prisma',
+            published: true,
+          },
+          {
+            title: 'Follow Nexus on Twitter',
+            content: 'https://twitter.com/nexusgql',
+            published: true,
+          },
+        ],
+      },
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
